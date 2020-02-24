3b49054d32bb9873dcdf1d471c34d115f7edc4d5
Anandadwipra <Anandadwipra@users.noreply.github.com>
2020-02-24 17:27:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import tkinter as tk
+
+root=tk.Tk()
+root.title("Kalkulator")
+root.geometry("500x300")
+root.config(bg="red")
+button=[]
+equation = tk.StringVar() 
+print(type(equation))
+# for x in range(0,10):
+
+# 	button.append(tk.Button(text=str(x),padx=30))
+# 	if x == 1:
+# 		button[x].pack(side="left")
+# 		print("a")	
+# 	button[x].pack()
+
+
+
+
+
+root.mainloop()
