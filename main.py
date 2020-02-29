0ffb3e6b04ff9e3ccbbc3b43d8f20d52b3d09298
Anandadwipra <Anandadwipra@users.noreply.github.com>
2020-03-01 06:45:01 +0700

    a

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
+from functools import partial
 import tkinter as tk
 
-root=tk.Tk()
-root.title("Kalkulator")
-root.geometry("500x300")
-root.config(bg="red")
-button=[]
-equation = tk.StringVar() 
-print(type(equation))
-# for x in range(0,10):
 
-# 	button.append(tk.Button(text=str(x),padx=30))
-# 	if x == 1:
-# 		button[x].pack(side="left")
-# 		print("a")	
-# 	button[x].pack()
+class applikasiKalkulator(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Kalkulator tkinter")
+        self.membuatTombol()
+        self.penentu = False
 
+    def membuatTombol(self):
+        self.layar = tk.Entry(self, width=25)
+        self.layar.grid(row=0, column=0, columnspan=5)
 
+        btn_list = [
+            '1', '2', '3',
+            '4', '5', '6',
+            '7', '8', '9',
+            '0', '+', '-',
+            'C', '/', '*',
+            '='
+        ] 
+        baris = 1
+        kolom = 0
+        for penampung in btn_list:
+            perintah = partial(self.hitung, penampung)
+            # print(perintah)
+            if penampung == '=':
+                tk.Button(self, text='=', width=22, command=perintah).grid(row=baris, column=kolom, columnspan=5)
+            else :
+                tk.Button(self, text=penampung, width=5, command=perintah).grid(row=baris, column=kolom)
+            kolom += 1
+            if kolom > 2:
+                kolom = 0
+                baris += 1
 
+    def hitung(self, key):
+        if key == '=':
+            self.penentu = True
+            try:
+                result = eval(self.layar.get())
+                self.layar.delete(0, tk.END)
+                self.layar.insert(tk.END, str(result))
+            except:
+                self.layar.insert(tk.END, "-> Error!")
+        elif key == 'C':
+            self.layar.delete(0, tk.END)
+        else:
+            if self.penentu :
+                self.layar.delete(0, tk.END)
+                self.penentu = False
+            self.layar.insert(tk.END, key)
 
-
-root.mainloop()
+panggil = applikasiKalkulator()
+panggil.mainloop()
